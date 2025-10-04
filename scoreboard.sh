5b5279c11db67609981d32cd19ded5e2de9c6552
Danylo Oparko <daniaoparko@mac.speedport.ip>
2025-10-04 18:01:23 +0200

    Script for adding users and their best to the scoreboard

---
+++ scoreboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#script for adding users and their best to the scoreboard
+#(might be deleted soon and moved to spiel.sh)
+free() {
+	echo
+}
+
+free
+echo -e "\t Enter your name:"
+read name
