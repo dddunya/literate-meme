0fc8c62124bfe3bb419b136fedb554413e7d95f2
Danylo Oparko <daniaoparko@mac.speedport.ip>
2025-10-05 12:50:57 +0200

    Still need to fix scoreboard player´s best update

--- spiel.sh
+++ spiel.sh
@@ -16,10 +16,8 @@ YELLOW='\033[1;33m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-#Game vars
-# num=$((RANDOM % 11))
-
-# tab='\t'
+#Tabs
+tab='\t'
 
 #Free line
 free() {
@@ -32,17 +30,17 @@ my_game() {
 	#Random number
 	num=$((RANDOM % 11))
 	free
-	echo -e "\t\t${NC}     Please enter your name to start"
+	echo -e "$tab$tab${NC}     Please enter your name to start"
 	free
 	read name
 	clear
 	free
-	echo -e "\t\t${YELLOW}     🎯 Guess a number between 1 and 10:"
+	echo -e "$tab$tab${YELLOW}     🎯 Guess a number between 1 and 10:"
 	read guess
 
 	##Loop for the right guess and attempts
 	while [[ "$guess" -ne "$num" ]]; do
-		attempts_new=$(($attempts + 1))
+		attempts=$(($attempts + 1))
 
 		if [[ "$guess" -lt "$num" ]]; then
 			echo -e "${RED}⬇ Too low! Try again."
@@ -62,47 +60,58 @@ my_game() {
 	done
 
 	free
-	echo -e "\t${GREEN}✅Correct! You guessed it in $attempts attempts!"
+	echo -e "$tab${GREEN}✅Correct! You guessed it in $attempts attempts!"
 	free
+
+	#Update scoreboard
+	if grep -q "^$name:" scoreboard.txt; then
+		sed -i '' "s/^$name:.*/$name:$attempts/" scoreboard.txt
+	else
+		if [[ "$attempts" -lt "$attempts" ]]; then
+			echo "$name:$attempts" >>scoreboard.txt
+		fi
+
+	fi
+	sort -t: -k1,1 scoreboard.txt -o scoreboard.txt
 
 }
 clear
 #Welcome screen
 free
-echo -e "\t${NC}##################################"
-echo -e "\t${NC}#                                #"
-echo -e "\t# ${YELLOW}Welcome to Guess the Number!🎲 ${NC}#"
-echo -e "\t${NC}#                                #"
-echo -e "\t${NC}##################################\n"
+echo -e "$tab${NC}##################################"
+echo -e "$tab${NC}#                                #"
+echo -e "$tab# ${YELLOW}Welcome to Guess the Number!🎲 ${NC}#"
+echo -e "$tab${NC}#                                #"
+echo -e "$tab${NC}##################################\n"
 
 #Rules
-echo -e "\t${NC} The rules are simple:"
+echo -e "$tab${NC} The rules are simple:"
 free #4 spaces
-echo -e "\t${NC}   - The computer will randomly pick a secret number."
-echo -e "\t${NC}   - Your job is to guess it as quickly as possible."
-echo -e "\t${NC}   - After each guess, you’ll be told if the number is higher or lower than your choice."
-echo -e "\t${NC}   - The fewer attempts you need, the better your score!"
+echo -e "$tab${NC}   - The computer will randomly pick a secret number."
+echo -e "$tab${NC}   - Your job is to guess it as quickly as possible."
+echo -e "$tab${NC}   - After each guess, you’ll be told if the number is higher or lower than your choice."
+echo -e "$tab${NC}   - The fewer attempts you need, the better your score!"
 free
-echo -e "\t${CYAN}     At the end of the game, your score will be recorded on the Scoreboard 🏆."
+echo -e "$tab${CYAN}     At the end of the game, your score will be recorded on the Scoreboard 🏆."
 
 #Option-play
-while true; do
+while true; do #ask Faysal
 	free
 	free
-	echo -e "\t${CYAN} Choose an option to begin:"
+	echo -e "$tab${CYAN} Choose an option to begin:"
 	free
-	echo -e "\t${YELLOW}     1. View Scoreboard 🏆"
-	echo -e "\t${CYAN}     2. Start Playing ▶️"
-	echo -e "\t${RED}     3. Exit ❌"
+	echo -e "$tab${YELLOW}     1. View Scoreboard 🏆"
+	echo -e "$tab${CYAN}     2. Start Playing ▶️"
+	echo -e "$tab${RED}     3. Exit ❌"
 	read option
 	case $option in
 	1)
 		clear
 		free
-		echo -e "\t${YELLOW} 🏆 Scoreboard:${NC}"
+		echo -e "$tab${YELLOW} 🏆 Scoreboard:${NC}"
 		free
 		free
-		bat --style=numbers scoreboard.txt
+		bat --style=numbers scoreboard.txt #Alternative to "cat"
 		;;
 	2)
 		clear
